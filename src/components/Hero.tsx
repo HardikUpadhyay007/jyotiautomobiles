42e19f2efb1c1367c9f09a092be9be7085cbe2d4
Hardik Upadhyay <119848774+HardikUpadhyay007@users.noreply.github.com>
2025-01-30 19:34:40 +0530

    imports updation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,4 @@
 import { Button } from "./ui/button";
-import { buttonVariants } from "./ui/button";
-import { HeroCards } from "./HeroCards";
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import hero2 from "../assets/hero2.png";
 export const Hero = () => {
     return (
