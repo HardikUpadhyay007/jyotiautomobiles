10c7902842671c3f77e2f477754224352f1a46fe
Hardik Upadhyay <119848774+HardikUpadhyay007@users.noreply.github.com>
2025-01-30 19:51:21 +0530

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,163 +1,3 @@
-// import { LogoIcon } from "./Icons";
-// import logo from "../assets/logo.svg";
-// export const Footer = () => {
-//     return (
-//         <footer id="footer">
-//             <hr className="w-11/12 mx-auto" />
-
-//             <section className="container py-20 grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
-//                 <div className="col-span-full  xl:col-span-2">
-//                     <a
-//                         rel="noreferrer noopener"
-//                         href="/"
-//                         className="font-bold text-xl flex"
-//                     >
-//                         <img
-//                             className="w-30 mr-4 h-20 bg-transparent hidden sm:block"
-//                             src={logo}
-//                         ></img>
-//                         <p className="mt-5 text-2xl">Jyoti Automobiles </p>
-//                     </a>
-//                 </div>
-
-//                 <div className="flex flex-col gap-2">
-//                     <h3 className="font-bold text-lg">Follow US</h3>
-//                     <div>
-//                         <a
-//                             rel="noreferrer noopener"
-//                             href="#"
-//                             className="opacity-60 hover:opacity-100"
-//                         >
-//                             Github
-//                         </a>
-//                     </div>
-
-//                     <div>
-//                         <a
-//                             rel="noreferrer noopener"
-//                             href="#"
-//                             className="opacity-60 hover:opacity-100"
-//                         >
-//                             Twitter
-//                         </a>
-//                     </div>
-
-//                     <div>
-//                         <a
-//                             rel="noreferrer noopener"
-//                             href="#"
-//                             className="opacity-60 hover:opacity-100"
-//                         >
-//                             Dribbble
-//                         </a>
-//                     </div>
-//                 </div>
-
-//                 <div className="flex flex-col gap-2">
-//                     <h3 className="font-bold text-lg">Platforms</h3>
-//                     <div>
-//                         <a
-//                             rel="noreferrer noopener"
-//                             href="#"
-//                             className="opacity-60 hover:opacity-100"
-//                         >
-//                             Web
-//                         </a>
-//                     </div>
-
-//                     <div>
-//                         <a
-//                             rel="noreferrer noopener"
-//                             href="#"
-//                             className="opacity-60 hover:opacity-100"
-//                         >
-//                             Mobile
-//                         </a>
-//                     </div>
-
-//                     <div>
-//                         <a
-//                             rel="noreferrer noopener"
-//                             href="#"
-//                             className="opacity-60 hover:opacity-100"
-//                         >
-//                             Desktop
-//                         </a>
-//                     </div>
-//                 </div>
-
-//                 <div className="flex flex-col gap-2">
-//                     <h3 className="font-bold text-lg">About</h3>
-//                     <div>
-//                         <a
-//                             rel="noreferrer noopener"
-//                             href="#"
-//                             className="opacity-60 hover:opacity-100"
-//                         >
-//                             Features
-//                         </a>
-//                     </div>
-
-//                     <div>
-//                         <a
-//                             rel="noreferrer noopener"
-//                             href="#"
-//                             className="opacity-60 hover:opacity-100"
-//                         >
-//                             Pricing
-//                         </a>
-//                     </div>
-
-//                     <div>
-//                         <a
-//                             rel="noreferrer noopener"
-//                             href="#"
-//                             className="opacity-60 hover:opacity-100"
-//                         >
-//                             FAQ
-//                         </a>
-//                     </div>
-//                 </div>
-
-//                 <div className="flex flex-col gap-2">
-//                     <h3 className="font-bold text-lg">Community</h3>
-//                     <div>
-//                         <a
-//                             rel="noreferrer noopener"
-//                             href="#"
-//                             className="opacity-60 hover:opacity-100"
-//                         >
-//                             Youtube
-//                         </a>
-//                     </div>
-
-//                     <div>
-//                         <a
-//                             rel="noreferrer noopener"
-//                             href="#"
-//                             className="opacity-60 hover:opacity-100"
-//                         >
-//                             Discord
-//                         </a>
-//                     </div>
-
-//                     <div>
-//                         <a
-//                             rel="noreferrer noopener"
-//                             href="#"
-//                             className="opacity-60 hover:opacity-100"
-//                         >
-//                             Twitch
-//                         </a>
-//                     </div>
-//                 </div>
-//             </section>
-//         </footer>
-//     );
-// };
-
-// import div from "./animation-container";
-import { Instagram, Youtube } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import logo from "../assets/logo.svg";
 const footerLink = [
@@ -228,7 +68,6 @@ export const Footer = () => {
                             rel="noopener noreferrer"
                         >
                             <span className="sr-only">Youtube</span>
-                            <Youtube className="h-5 w-5" />
                         </a>
                     </Button>
                     <Button variant="ghost" size="icon" asChild>
@@ -238,7 +77,6 @@ export const Footer = () => {
                             rel="noopener noreferrer"
                         >
                             <span className="sr-only">Instagram</span>
-                            <Instagram className="h-5 w-5" />
                         </a>
                     </Button>
                 </div>
